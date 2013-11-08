a31be8e04c16f535b0b2290f10502e697609fd05
Michel Gotta <michel.gotta@4wdmedia.de>
2013-11-08 17:53:42 +0100

    added IE10 support. Prefix only when used...

--- js/script.js
+++ js/script.js
@@ -7,22 +7,18 @@ jQuery(document).ready(function($, undefined) {
 
 	var mainWidth = 0;
 	var time = 3.0;
-	var plattform;
 	var canvas = document.getElementById("canvas");
 	var miniCanvas = document.getElementById("track-bezier-canvas");
 	var miniCanvasLinear = document.getElementById("track-linear-canvas");
 	var ctx;
 	var mctx;
 	var $car = $('.car');
-	
-	if ($('html').hasClass('canvas') && $('html').hasClass('cssanimations')) {
-		
-		// check which plattform
-		if ($.browser.webkit) {
-			plattform = "webkit";
-		} else if($.browser.mozilla) {
-			plattform = "moz";
-		}
+	var transitionPrefixed;
+
+	// alert($.browser.msie);
+
+
+	if ( Modernizr.canvas && Modernizr.cssanimations ) {
 
 		mainWidth = $('#curve-creator').width();
 
@@ -66,25 +62,25 @@ jQuery(document).ready(function($, undefined) {
 		});
 
 		$('#animation-type-selector').change(function() {
-			switch($(this).val()) {
-				case "1": 
+			switch( $(this).val() ) {
+				case "1":
 					$car.removeClass('wide');
 					break;
 			}
 		});
 
-		if (miniCanvasLinear.getContext) {
+		if ( miniCanvasLinear.getContext ) {
 			var ctxl = miniCanvasLinear.getContext('2d');
 			ctxl.strokeStyle = "#8AAAB2";
 			ctxl.lineWidth	 = 2;
 			ctxl.beginPath();
-			ctxl.moveTo(0,40);
-			ctxl.lineTo(40,0);
+			ctxl.moveTo(0, 40);
+			ctxl.lineTo(40, 0);
 			ctxl.stroke();
 			ctxl.closePath();
 		}
 
-		draw(mainWidth*0.75,mainWidth*0.75,mainWidth*0.25,mainWidth*0.25);
+		draw(mainWidth * 0.75, mainWidth * 0.75, mainWidth * 0.25, mainWidth * 0.25);
 	} else {
 		$('#container').html("");
 		$('#warning').show();
@@ -93,20 +89,21 @@ jQuery(document).ready(function($, undefined) {
 	function getPositionsAndDraw() {
 		var e = $('#bezier-control-2').position();
 		var o = $('#bezier-control-1').position();
-		draw(o.left+16, o.top+16, e.left+16, e.top+16);
+		draw(o.left + 16, o.top + 16, e.left + 16, e.top + 16);
 	}
 
 	function drawLine(x, y, start) {
 		ctx.strokeStyle = "#666"
 		ctx.lineWidth = 6;
-		
+
 		ctx.beginPath();
-		if (start) {
+
+		if ( start ) {
 			ctx.moveTo(mainWidth, 0);
 		} else {
 			ctx.moveTo(0, mainWidth);
 		}
-		
+
 		ctx.lineTo(x, y);
 		ctx.stroke();
 		ctx.closePath();
@@ -123,8 +120,8 @@ jQuery(document).ready(function($, undefined) {
 		target.closePath();
 	}
 
-	function draw(x1,y1,x2,y2) {
-		if (canvas.getContext) {
+	function draw(x1, y1, x2, y2) {
+		if ( canvas.getContext ) {
 			ctx = canvas.getContext("2d");
 			canvas.width = canvas.width; // refresh hack
 
@@ -135,38 +132,39 @@ jQuery(document).ready(function($, undefined) {
 			// Draw bezier
 			drawBezier(ctx, {x1: x1, y1: y1, x2: x2, y2: y2}, 12, mainWidth);
 		}
-		
+
 		// Draw on curve on mini canvas
-		if (miniCanvas.getContext) {
+		if ( miniCanvas.getContext ) {
 			mctx = miniCanvas.getContext("2d");
 			miniCanvas.width = miniCanvas.width; // refresh hack
-			
+
 			// Get bezier control coordinates for small canvas (x * width of canvas / width of big canvas)
 			var pos = {
-				x1: x1*40/mainWidth, 
-				y1: y1*40/mainWidth, 
-				x2: x2*40/mainWidth, 
-				y2: y2*40/mainWidth
+				x1: x1 * 40 / mainWidth,
+				y1: y1 * 40 / mainWidth,
+				x2: x2 * 40 / mainWidth,
+				y2: y2 * 40 / mainWidth
 			};
-			
+
 			// Draw on small canvas
 			drawBezier(mctx, pos, 2, 40);
 		}
 
 		// Make values CSS ready...
-		x1 = x1/mainWidth;
-		y1 = 1-y1/mainWidth;
-		x2 = x2/mainWidth;
-		y2 = 1-y2/mainWidth;
+		x1 = x1 / mainWidth;
+		y1 = 1 - y1 / mainWidth;
+		x2 = x2 / mainWidth;
+		y2 = 1 - y2 / mainWidth;
 
-		var cssOutput = '-'+plattform+'-transition: all '+time.toFixed(1)+'s cubic-bezier(' + x1.toFixed(2) + ', ' + y1.toFixed(2) +', '+ x2.toFixed(2) + ', ' + y2.toFixed(2) + ')';
+		transitionPrefixed = Modernizr.prefixed('transition').replace(/([A-Z])/g, function(str,m1){ return '-' + m1.toLowerCase(); }).replace(/^ms-/,'-ms-');
+
+		var cssOutput = transitionPrefixed + ': all ' + time.toFixed(1) + 's cubic-bezier(' + x1.toFixed(2) + ', ' + y1.toFixed(2) + ', ' + x2.toFixed(2) + ', ' + y2.toFixed(2) + ')';
 
 		// Build the copy&paste part
 		$('#output').html(cssOutput);
-		
+
 		// Animate the cars
 		$car.attr('style', cssOutput);
-		$('.car-linear').attr('style', '-'+plattform+'-transition: all '+time+'s linear');
+		$('.car-linear').attr('style', transitionPrefixed + ': all ' + time + 's linear');
 	}
-
 });
